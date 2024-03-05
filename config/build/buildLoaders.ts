9db43af15ad7cfa125a8acc0e9ab2a0233aa5e4d
Kamchybekk <manasbekoovich@gmail.com>
2024-03-05 12:09:54 +0600

    save

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,7 +6,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-  }; 
+  };
 
   const babelLoader = {
     test: /\.(js|jsx|tsx)$/,
@@ -62,6 +62,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       },
     ],
   };
-
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, typescriptLoader, cssLoader, babelLoader];
 }
