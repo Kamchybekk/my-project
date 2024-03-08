e2809c6c4334a5a9b5659088a79ab62bb6273cbc
Kamchybekk <manasbekoovich@gmail.com>
2024-03-08 23:36:38 +0600

    tried set test for button

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
+import { Button, ThemeButton } from './Button';
 
 describe('classNames', () => {
   test('with only first param', () => {
     render(<Button>TEST</Button>);
     expect(screen.getByText('TEST')).toBeInTheDocument();
+  });
+
+  test('with only first param', () => {
+    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+    expect(screen.getByText('TEST')).toHaveClass('clear');
+    screen.debug();
   });
 });
