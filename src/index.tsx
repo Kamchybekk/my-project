8a08aa273a9d9e77fead0b76dde649a052d6a448
Kamchybekk <manasbekoovich@gmail.com>
2024-03-14 16:54:30 +0600

    save

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import {ThemeProvider} from './app/providers/ThemeProvider/'
 import App from './app/App';
-
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
-import React from 'react';
 
 render(
     <BrowserRouter>
