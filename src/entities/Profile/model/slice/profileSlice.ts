242e0d925eff91ce12cd1b50ab96df7f2185b9b2
Kamchybekk <manasbekoovich@gmail.com>
2024-04-10 22:13:18 +0600

    created new async thunk function updateProfileData

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Profile, ProfileSchema } from '../types/profile';
-import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData/fetchProfileData';
 
 const initialState: ProfileSchema = {
     readonly: true,
